e434e97bc37e4f8ab04d5723806fea01d2db897b
DylanLeporcq <56022443+DylanLeporcq@users.noreply.github.com>
2020-02-10 15:04:12 +0100

    JS : Add the statement functions. Available = green, Busy = orange, Not here = red;

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ function notAvailableNow(x) {
 for (i=0; i<document.getElementsByClassName('N/A').length; i++) {
     notAvailableNow(i);
 }
+
+function stateAppareance(x) {
+    stateAll = document.getElementsByClassName('etat')[x];
+    if (stateAll.innerHTML === "Disponible"){
+        stateAll.style.color="#78ff00";
+        stateAll.style.border="1px solid #78ff00";
+    }
+    else if (stateAll.innerHTML === "Occupé"){
+        stateAll.style.color="#ffc42d";
+        stateAll.style.border="1px solid #ffc42d";
+    }
+    else if (stateAll.innerHTML === "Absent"){
+        stateAll.style.color="#ff3520";
+        stateAll.style.border="1px solid #ff3520";
+    }
+}
+
+for (i=0; i<document.getElementsByClassName('etat').length; i++) {
+    stateAppareance(i);
+}
